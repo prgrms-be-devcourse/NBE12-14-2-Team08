8372962172bf1a8ee3c60d3f46168d7f9804efc7
swshindev-beep <swshin.dev@gmail.com>
2026-09-17 14:36:14 +0900

    fix: passwordEncoder 오류 수정

--- backend/src/main/java/com/back/BackendApplication.java
+++ backend/src/main/java/com/back/BackendApplication.java
@@ -1,30 +1,14 @@
 package com.back;
 
-import com.back.domain.group.service.PasswordEncoder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
-	}
-
-	// 임시 시큐리티
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new PasswordEncoder() {
-			@Override
-			public String encode(CharSequence rawPassword) {
-				return rawPassword.toString();
-			}
-
-			@Override
-			public boolean matches(CharSequence rawPassword, String encodedPassword) {
-				return rawPassword.toString().equals(encodedPassword);
-			}
-		};
 	}
 }
