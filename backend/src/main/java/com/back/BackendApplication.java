34319e09ca2619a01303044ecac8ffc14b551746
swshindev-beep <swshin.dev@gmail.com>
2026-09-17 15:13:44 +0900

    chore: 불필요한 파일 및 import 정리 (#52)

--- backend/src/main/java/com/back/BackendApplication.java
+++ backend/src/main/java/com/back/BackendApplication.java
@@ -2,8 +2,6 @@ package com.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class BackendApplication {
